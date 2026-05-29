5b1f7092a228bf4edffd11200dd8863dbf412024
Panchuet <stralucit@gmail.com>
2026-05-29 14:14:12 +0800

    Add files via upload

---
+++ SimpleFireBurst.lua
@@ -0,0 +1,138 @@
+behaviour("SimpleFireBurst")
+
+-- Keys:
+--   startNonBurst         (bool)   : If true, the weapon starts in non-burst mode instead of burst mode
+--   nonBurstAuto          (bool)   : If true, non-burst mode will fire in full-auto instead of semi-auto
+--   nonResetting          (bool)   : If true, leftover shots carry over to the next trigger pull (M16A2 style)
+--   burstCount            (int)    : Total number of shots fired per burst
+--   botBurstCooldown      (float)  : Cooldown time in seconds between bursts specifically for bots
+--   switchCooldown        (float)  : Lockout time in seconds before you can switch modes again, best if you match the switch animation length
+--   switchKeybind         (string) : Lowercase keybind of the key you want to use to switch modes
+--   switchParameterName   (string) : Name of the animator trigger parameter to play on switch
+--   selectorValues        (string) : Two space-separated integers for the selector lever position per mode e.g. "0 1" (mode 0 = burst, mode 1 = non-burst)
+--   selectorParameterName (string) : Name of the animator integer parameter that holds the selector lever position
+
+function SimpleFireBurst:Start()
+    self.weapon = self.gameObject.GetComponent(Weapon)
+    self.animator = self.gameObject.GetComponent(Animator)
+    self.dataContainer = self.gameObject.GetComponent(DataContainer)
+
+    self.modeIndex = self.dataContainer.GetBool("startNonBurst") and 1 or 0
+    self.nonBurstAuto = self.dataContainer.GetBool("nonBurstAuto")
+    self.nonResetting = self.dataContainer.GetBool("nonResetting")
+
+    self.burstCount = self.dataContainer.GetInt("burstCount")
+    self.botBurstCooldown = self.dataContainer.GetFloat("botBurstCooldown")
+
+    self.switchCooldownDuration = self.dataContainer.GetFloat("switchCooldown")
+    self.switchKeybind = self.dataContainer.GetString("switchKeybind")
+
+    self.selectorValues = {}
+    for match in (self.dataContainer.GetString("selectorValues") .. " "):gmatch("(.-) ") do
+        table.insert(self.selectorValues, tonumber(match))
+    end
+
+    if self.animator ~= nil then
+        self.switchParameter = self.animator.StringToHash(self.dataContainer.GetString("switchParameterName"))
+        self.selectorParameter = self.animator.StringToHash(self.dataContainer.GetString("selectorParameterName"))
+    end
+
+    self.switchTimer = 0
+    self.shotsFired = 0
+    self.botCooldownTimer = 0
+    self.burstLocked = false
+    
+    if self.weapon ~= nil then
+        self.weapon.onSpawnProjectiles.AddListener(self, "OnFire")
+    end
+
+    self:ApplyMode()
+end
+
+function SimpleFireBurst:OnFire()
+    if self.modeIndex ~= 0 then return end
+
+    self.shotsFired = self.shotsFired + 1
+    if self.shotsFired >= self.burstCount then
+        self.weapon.LockWeapon()
+        self.burstLocked = true
+        
+        if self.weapon.user ~= nil and self.weapon.user.isBot then
+            self.botCooldownTimer = self.botBurstCooldown
+        end
+    end
+end
+
+function SimpleFireBurst:ApplyMode()
+    self.weapon.isAuto = self.modeIndex == 0 and true or self.nonBurstAuto
+    
+    if self.animator ~= nil then
+        self.animator.SetInteger(self.selectorParameter, self.selectorValues[self.modeIndex + 1])
+    end
+    
+    self.shotsFired = 0
+    self.burstLocked = false
+    self.botCooldownTimer = 0
+    self.weapon.UnlockWeapon()
+end
+
+function SimpleFireBurst:SwitchMode()
+    self.modeIndex = 1 - self.modeIndex
+    self.switchTimer = self.switchCooldownDuration
+    
+    if self.animator ~= nil then
+        self.animator.SetTrigger(self.switchParameter)
+    end
+    
+    self:ApplyMode()
+end
+
+function SimpleFireBurst:OnEnable()
+    if self.animator == nil then return end
+    self.animator.SetInteger(self.selectorParameter, self.selectorValues[self.modeIndex + 1])
+end
+
+function SimpleFireBurst:Update()
+    if self.weapon == nil then return end 
+
+    if self.switchTimer > 0 then
+        self.switchTimer = self.switchTimer - Time.deltaTime
+    end
+
+    local isEquippedByPlayer = (self.weapon.user ~= nil and self.weapon.user.isPlayer and self.weapon.isUnholstered)
+
+    if isEquippedByPlayer
+        and Input.GetKeyDown(self.switchKeybind)
+        and self.switchTimer <= 0
+        and not self.weapon.isReloading
+    then
+        self:SwitchMode()
+    end
+
+    if self.modeIndex == 0 then
+        local triggerReleased = not self.weapon.isHoldingFire
+        local isBot = (self.weapon.user ~= nil and self.weapon.user.isBot)
+
+        if isBot then
+            if self.burstLocked then
+                if self.botCooldownTimer > 0 then
+                    self.botCooldownTimer = self.botCooldownTimer - Time.deltaTime
+                else
+                    self.weapon.UnlockWeapon()
+                    self.burstLocked = false
+                    self.shotsFired = 0
+                end
+            end
+        else
+            if self.burstLocked and triggerReleased then
+                self.weapon.UnlockWeapon()
+                self.burstLocked = false
+                self.shotsFired = 0
+            end
+        end
+
+        if not self.nonResetting and triggerReleased then
+            self.shotsFired = 0
+        end
+    end
+end
